b5ddff74d67b72cccb9a82016dd1fdf6aa9a8b1e
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-07-07 23:23:19 +0800

    Create component Nav - Add temporary content

---
+++ components/navigation/Nav.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import navStyles from "../../styles/navigation/Nav.module.css";
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul className={navStyles.container}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/categories">Categories</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
