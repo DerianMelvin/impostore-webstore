3d7f961bed7f3124ac56bd77ce9a6483a1392194
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-07-07 23:26:52 +0800

    Create component Layout - Import component Nav - Add children prop to main element

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+import Nav from './navigation/Nav';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Nav />
+      <div>
+        <main>{children}</main>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
