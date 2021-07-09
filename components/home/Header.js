2ed6b92e4acfb6a7295ad5a54ca4d34a19efe4d8
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-07-09 22:10:10 +0800

    Create component Header - Import Header.module.css

---
+++ components/home/Header.js
@@ -0,0 +1,12 @@
+import styles from "../../styles/Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.container}>
+      <h1>Made to fit your style</h1>
+      <p>Explore our wide range of products</p>
+    </header>
+  );
+};
+
+export default Header;
