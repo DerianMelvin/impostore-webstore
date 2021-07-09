4801f162cc455a102892aebf1bce98b306708f30
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-07-09 21:36:07 +0800

    Rename Item.js to ItemList - Adjust element classNames & styling props

---
+++ components/home/ItemList.js
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import styles from "../../styles/Products.module.css";
+
+const Item = ({ products }) => {
+  return (
+    <div className={styles.container}>
+      {products.map((product) => {
+        return (
+          <div key={product.id} className={styles.card}>
+            <Image
+              src={product.image}
+              alt={product.title}
+              width="300"
+              height="300"
+            />
+            <h3 className={styles.title}>{product.title}</h3>
+            <div className={styles.price}>
+              <h4>$ {product.price}</h4>
+              <h4>USD</h4>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Item;
