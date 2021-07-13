8d9bc5a3ede55af44554908c7f3ecf890eedb23a
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-07-13 15:25:02 +0800

    Rename /home to /products - Adjust import path on affected pages

--- pages/category/[categoryName].js
+++ pages/category/[categoryName].js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Header from '../../components/Header'
-import ProductList from "../../components/home/ProductList";
+import ProductList from "../../components/products/ProductList";
 
 const categoryName = ({ products }) => {
   // Get current category of products
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
-import ProductList from "../components/home/ProductList";
+import ProductList from "../components/products/ProductList";
 
 export default function Home({ products }) {
   return (
