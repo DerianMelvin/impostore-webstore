560cfa7c9cecf276d7d3c1c841b439838a871410
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-07-13 15:03:21 +0800

    Create page [categoryName].js - Import Head from next/head - Import component Header - Import component ItemList - Add getStaticPaths function - Add getStaticProps function

---
+++ pages/category/[categoryName].js
@@ -0,0 +1,55 @@
+import Head from "next/head";
+import Header from '../../components/Header'
+import ItemList from "../../components/home/ItemList";
+
+const categoryName = ({ products }) => {
+  // Get current category of products
+  const category = products[0].category;
+
+  return (
+    <div>
+      <Head>
+        <title>{`Some Store | ${category}`}</title>
+      </Head>
+
+      <Header
+        heading={category}
+        description={`Select from our wide selection of ${category}`}
+      />
+      <ItemList products={products} />
+    </div>
+  );
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch("https://fakestoreapi.com/products/categories");
+  const categories = await res.json();
+
+  const paths = categories.map((category) => {
+    return {
+      params: {
+        categoryName: category,
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const res = await fetch(
+    `https://fakestoreapi.com/products/category/${params.categoryName}`
+  );
+  const products = await res.json();
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
+
+export default categoryName;
