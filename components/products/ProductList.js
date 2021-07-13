13fef61b45e1f718ab05634a61c1a63719d1be63
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-07-13 15:48:14 +0800

    Import Link from next/link - Add links to each product

--- components/products/ProductList.js
+++ components/products/ProductList.js
@@ -1,24 +1,27 @@
 import Image from "next/image";
 import styles from "../../styles/Products.module.css";
+import Link from 'next/link';
 
 const ProductList = ({ products }) => {
   return (
     <div className={styles.container}>
       {products.map((product) => {
         return (
-          <div key={product.id} className={styles.card}>
-            <Image
-              src={product.image}
-              alt={product.title}
-              width="300"
-              height="300"
-            />
-            <h3 className={styles.title}>{product.title}</h3>
-            <div className={styles.price}>
-              <h4>$ {product.price}</h4>
-              <h4>USD</h4>
+          <Link href={`/product/${product.id}`} key={product.id} passHref>
+            <div className={styles.card}>
+              <Image
+                src={product.image}
+                alt={product.title}
+                width="300"
+                height="300"
+              />
+              <h3 className={styles.title}>{product.title}</h3>
+              <div className={styles.price}>
+                <h4>$ {product.price}</h4>
+                <h4>USD</h4>
+              </div>
             </div>
-          </div>
+          </Link>
         );
       })}
     </div>
